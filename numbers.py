3f9e4910787cf2d5f0730b6cc841023432ee2b23
TheTrain242 <35146770+TheTrain242@users.noreply.github.com>
2019-04-18 20:26:38 +0100

    created the numbers game

---
+++ numbers.py
@@ -0,0 +1,34 @@
+# this is a guess the number game
+import random
+
+guessesTaken = 0
+
+print("Hello! What's your name?")
+playerName = input()
+
+number = random.randint(1, 20)
+print("well, " + playerName + " I'm thinking of a number between 1 and 20")
+
+while guessesTaken < 6:
+    print("Take a guess!")
+    guess = input()
+    guess = int(guess)
+
+    guessesTaken = guessesTaken + 1
+
+    if guess < number:
+        print("Your guess is too low")
+
+    if guess > number:
+        print("Your guess is too high")
+
+    if guess == number:
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken)
+    print("Good Job " + playerName + "! You guessed the number in " + guessesTaken + " guesses!")
+
+if guess != number:
+    number = str(number)
+    print("Nope. The number I was thinking of was " + number)
