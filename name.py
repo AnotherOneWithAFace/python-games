286bfbadbc8f08d4ac88c1f149c19a33c79e5c7f
TheTrain242 <35146770+TheTrain242@users.noreply.github.com>
2019-04-18 20:55:06 +0100

    renamed the name game

---
+++ name.py
@@ -0,0 +1,5 @@
+# This programme says hello and then asks for my name
+print('Hello!')
+print("What's your name?")
+name = input()
+print("Nice to meet you, " + name + '!\n')
--- razist.py
+++
@@ -1,5 +0,0 @@
-# This programme says hello and then asks for my name
-print('Hello!')
-print("What's your name?")
-name = input()
-print("Nice to meet you, " + name + '!\n')
