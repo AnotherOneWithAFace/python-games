7358305b06410732d211b340d76f38e5d66bb030
SchloopyPeen <35146770+AnotherOneWithAFace@users.noreply.github.com>
2019-04-18 19:47:41 +0100

    initial commit to remote
    need 2 figure out how to upload directories

---
+++ favs.py
@@ -0,0 +1,16 @@
+# this programme asks you for your favourite stuff and then tells you it
+
+# fav colour
+print("Tell me what your favourite colour is\n")
+favcolour = input()
+
+# fav animal
+print("Tell me what your favourite animal is\n")
+favanimal = input()
+
+# fav food
+print("Tell me what your favourite food is\n")
+favfood = input()
+
+# display all this jucy data
+print("You have entered:\n -> Favourite Colo(u)r: " + favcolour +'\n' " -> Favourite Animal: " + favanimal +'\n' " -> Favourite Food: " + favfood +'\n')
