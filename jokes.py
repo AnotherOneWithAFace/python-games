1c80892928119dbf8897870f446246acfc0fff76
TheTrain242 <35146770+TheTrain242@users.noreply.github.com>
2019-04-23 21:00:40 +0100

    added jokes game

---
+++ jokes.py
@@ -0,0 +1,18 @@
+# this programm tells jokes. pretty simple
+
+print ("what do you get when you cross a snowman with a vampire?")
+input()
+print("frostbite!")
+print()
+print("what do dentists call an astronaut's cavity?")
+input()
+print("a black hole!")
+print()
+print("knock knock")
+input()
+print("who's there?")
+input()
+print("interupting cow")
+input()
+print("interupting cow wh")
+print("MOOOOO!")
