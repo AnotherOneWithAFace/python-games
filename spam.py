48f21c9e2d7ae42c03ce33bbc695f15ce049cfcb
TheTrain242 <35146770+TheTrain242@users.noreply.github.com>
2019-05-18 19:14:24 +0100

    made a spam file to test out the difference between local and global scope

---
+++ spam.py
@@ -0,0 +1,9 @@
+def spam(myName):
+    print("Hello, " + myName)
+    myName = "Waffles"
+    print("Your new name is " + myName)
+    print("\n\n")
+
+myName = "Albert"
+spam(myName)
+print("Howdy " + myName)
