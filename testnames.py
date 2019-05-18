e0311a13d9fc964412e2ce4629c95b1f01e78cb5
TheTrain242 <35146770+TheTrain242@users.noreply.github.com>
2019-05-18 19:04:57 +0100

    made sayHello function better with automatic spacing

--- testnames.py
+++ testnames.py
@@ -1,8 +1,8 @@
 def sayHello(name):
-    print("Hello" + name + "!")
+    print("Hello, " + name + "!")
 
 print("Say hello to Alice")
-fizzy = " Alice"
+fizzy = "Alice"
 sayHello(fizzy)
 print("Don't forget about Bob!")
-sayHello(" Bob")
+sayHello("Bob")
