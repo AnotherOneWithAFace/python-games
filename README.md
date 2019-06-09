821b527c0cd35ce386bc14106ab07eb00d1affd1
TheTrain242 <35146770+TheTrain242@users.noreply.github.com>
2019-06-09 11:18:12 +0100

    added license header to all files, and updated README

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ games and other minor programs I """made""" while learning python (from a book)
 
 ## Usage
 These programs only work with python3, not 2
+
+## License
+Licensed under the EUPL v1.2; see LICENSE.txt for details
--- dragon.py
+++ dragon.py
@@ -1,3 +1,23 @@
+#Copyright 2019 Matthew Sweeney
+#
+# Licensed under the EUPL, Version 1.1 or – as soon they
+#will be approved by the European Commission - subsequent
+#versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with the
+#Licence.
+# You may obtain a copy of the Licence at:
+#
+# https://joinup.ec.europa.eu/software/page/eupl5
+#
+# Unless required by applicable law or agreed to in
+#writing, software distributed under the Licence is
+#distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+#express or implied.
+# See the LICENSE.txt for the specific language governing
+#permissions and limitations under the Licence.
+
+
 # this program takes the player down into a world of dragons. a very small world
 
 import random
