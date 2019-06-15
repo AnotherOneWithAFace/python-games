5a79cdf9ef5d0653eae18a3f1aa087d27f1a0480
TheTrain242 <35146770+TheTrain242@users.noreply.github.com>
2019-06-15 16:47:45 +0100

    fixed license header; it's now all properly indented

--- func.py
+++ func.py
@@ -1,21 +1,21 @@
-#Copyright 2019 Matthew Sweeney
+# Copyright 2019 Matthew Sweeney
 #
 # Licensed under the EUPL, Version 1.2 or – as soon they
-#will be approved by the European Commission - subsequent
-#versions of the EUPL (the "Licence");
+# will be approved by the European Commission - subsequent
+# versions of the EUPL (the "Licence");
 # You may not use this work except in compliance with the
-#Licence.
+# Licence.
 # You may obtain a copy of the Licence at:
 #
 # https://joinup.ec.europa.eu/software/page/eupl5
 #
 # Unless required by applicable law or agreed to in
-#writing, software distributed under the Licence is
-#distributed on an "AS IS" basis,
+# writing, software distributed under the Licence is
+# distributed on an "AS IS" basis,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-#express or implied.
+# express or implied.
 # See the LICENSE.txt for the specific language governing
-#permissions and limitations under the Licence.
+# permissions and limitations under the Licence.
 
 
 import time
